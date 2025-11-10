882bc4ee831122e305071d01e205e0c935758255
Leshabanellh-png <PC@DESKTOP-26A9125>
2025-11-10 08:33:53 +0200

    Add project files.

---
+++ RazorPagesMovie1/Pages/Movies/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using RazorPagesMovie1.Data;
+
+namespace RazorPagesMovie1.Pages.Movies
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorPagesMovie1.Data.RazorPagesMovie1Context _context;
+
+        public DeleteModel(RazorPagesMovie1.Data.RazorPagesMovie1Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie is not null)
+            {
+                Movie = movie;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FindAsync(id);
+            if (movie != null)
+            {
+                Movie = movie;
+                _context.Movie.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ RazorPagesMovie1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using RazorPagesMovie1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorPagesMovie1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMovie1Context") ?? throw new InvalidOperationException("Connection string 'RazorPagesMovieContext' not found.")));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages();
+
+app.Run();
