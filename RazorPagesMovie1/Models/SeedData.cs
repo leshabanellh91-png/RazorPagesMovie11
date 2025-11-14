a50f78b76df14f37d2b74c4e51c7c6689780c8a7
Leshabanellh-png <PC@DESKTOP-26A9125>
2025-11-14 11:23:23 +0200

    database was displaying only two movies, had to add four movies on seeddata thereafter deleted database on sql server explorer , after cleaned solution,rebuilded solution then added update database command.

--- RazorPagesMovie1/Models/SeedData.cs
+++ RazorPagesMovie1/Models/SeedData.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using RazorPagesMovie.Models;
 using RazorPagesMovie1.Data;
 using RazorPagesMovie1.Models;
+using System.IO;
 
 namespace RazorPagesMovie1.Models
 {
@@ -29,7 +30,11 @@ namespace RazorPagesMovie1.Models
                 if (!context.Movie.Any())
                 {
                     var robReiner = context.Director.First(d => d.Name == "Rob Reiner");
-                    var nolan = context.Director.First(d => d.Name == "Christopher Nolan");
+                    var MoriswiSimon = context.Director.First(d => d.Name == "Moriswi Simon");
+                    var LeratoLee = context.Director.First(d => d.Name == "Lerato Lee");
+                    var LucyMmasa = context.Director.First(d => d.Name == "Lucy Mmasa");
+                    var MphoNkuna = context.Director.First(d => d.Name == "Mpho Nkuna");
+                    var MothibaFortunate = context.Director.First(d => d.Name == "Mothiba Fortunate");
 
                     context.Movie.AddRange(
                         new Movie
@@ -46,8 +51,40 @@ namespace RazorPagesMovie1.Models
                             ReleaseDate = DateTime.Parse("2010-07-16"),
                             Genre = "Sci-Fi",
                             Price = 9.99M,
-                            DirectorId = nolan.Id
-                        }
+                            DirectorId = MoriswiSimon.Id
+                        },
+                        new Movie
+                        {
+                            Title = "E.T. the Extra-Terrestrial",
+                            Genre = "Science Fiction",
+                            ReleaseDate = new DateTime(1982, 6, 11),
+                            Price = 7.99M,
+                            DirectorId = LeratoLee.Id
+                        },
+                         new Movie
+                         {
+                             Title = "Interstellar",
+                             Genre = "Science Fiction",
+                             ReleaseDate = new DateTime(2014, 11, 7),
+                             Price = 12.99M,
+                             DirectorId = LucyMmasa.Id
+                         },
+                    new Movie
+                    {
+                        Title = "Pulp Fiction",
+                        Genre = "Crime",
+                        ReleaseDate = new DateTime(1994, 10, 14),
+                        Price = 8.99M,
+                        DirectorId = MphoNkuna.Id
+                    },
+                    new Movie
+                    {
+                        Title = "Django Unchained",
+                        Genre = "Western",
+                        ReleaseDate = new DateTime(2012, 12, 25),
+                        Price = 11.99M,
+                        DirectorId = MothibaFortunate.Id
+                    }
                     );
 
                     context.SaveChanges();
