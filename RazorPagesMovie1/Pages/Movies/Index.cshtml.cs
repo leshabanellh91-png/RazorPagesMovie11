0bda5873f54412cb88ba32fac125cf2334b472f3
Leshabane Lerato <leshabanellh91@gmail.com>
2025-11-25 08:56:49 +0200

    updated index.cshtml, index.cshtml.cs  for displaying actors and directors for each movie.

--- RazorPagesMovie1/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie1/Pages/Movies/Index.cshtml.cs
@@ -20,30 +20,29 @@ namespace RazorPagesMovie1.Pages.Movies
             _context = context;
         }
 
-        // Selected genre from the query string
         [BindProperty(SupportsGet = true)]
         public string MovieGenre { get; set; }
 
-        // SelectList used by the <select>
         public SelectList Genre { get; set; }
 
-        // Search string from the query string
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
 
-        // Movies displayed on the page
         public IList<Movie> Movies { get; set; }
 
         public async Task OnGetAsync()
         {
-            // Query genres
+            // Query genres for dropdown
             IQueryable<string> genreQuery = _context.Movie
                 .OrderBy(m => m.Genre)
                 .Select(m => m.Genre)
                 .Distinct();
 
-            // Base query
-            var movies = _context.Movie.AsQueryable();
+            // Load movies including Actor and Director
+            var movies = _context.Movie
+                .Include(m => m.Actor)
+                .Include(m => m.Director)
+                .AsQueryable();
 
             // Filter by title
             if (!string.IsNullOrEmpty(SearchString))
@@ -57,7 +56,6 @@ namespace RazorPagesMovie1.Pages.Movies
                 movies = movies.Where(m => m.Genre == MovieGenre);
             }
 
-            // Load results into model
             Genre = new SelectList(await genreQuery.ToListAsync());
             Movies = await movies.ToListAsync();
         }
