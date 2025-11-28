71732af7bb052d78cb0231c05bcc3880cfee7b37
Leshabane Lerato <leshabanellh91@gmail.com>
2025-11-28 11:06:28 +0200

    added  trail  for movies in create and edit.cshtml, movie class.

---
+++ RazorPagesMovie1/Migrations/20251128083744_AddTrailerUrlToMovie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesMovie1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrailerUrlToMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrailerUrl",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrailerUrl",
+                table: "Movie");
+        }
+    }
+}
